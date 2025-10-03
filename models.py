e00d627dc87eb1a2bfdf71664dc721a05cb32ee6
Harsh Anand <harsh25061999@gmail.com>
2025-10-03 13:57:59 +0530

    Comment class added

--- models.py
+++ models.py
@@ -9,6 +9,7 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, index=True)
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+    comments = db.relationship('Comment', backref='author', lazy='dynamic')
 
     def set_password(self, password):
         # Specify a stable password hashing method to avoid environments
@@ -41,3 +42,10 @@ class Post(db.Model):
             'timestamp': self.timestamp.strftime('%Y-%m-%d %H:%M:%S'),
             'author_id': self.author_id
         }
+
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.Text)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    author_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id'))
